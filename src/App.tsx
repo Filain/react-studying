496cc5489a6d8fd3b9968ef6e8d7841dfc3760da
Filain <ratecurrent@gmail.com>
2024-05-31 14:01:48 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {FC, PropsWithChildren} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IProps extends PropsWithChildren {
+
 }
 
-export default App;
+const App: FC<IProps> = () => {
+    return (
+        <div>
+          {/*          1 ) npx create-react-app . --template typescript - створить проект  в пустому*/}
+          {/*          2) .gitignore додаємо .idea*/}
+          {/*          3) Видаляємо:*/}
+          {/*                        src/App.css*/}
+          {/*                        src/App.test.tsx*/}
+          {/*                        src/index.css - тут під вопросом, але видаляємо*/}
+          {/*                        src/logo.svg*/}
+          {/*                        src/reportWebVitals.ts*/}
+          {/*                        src/setupTests.ts*/}
+          {/*          4) src/index.tsx*/}
+          {/*                 root.render(<App/>);*/}
+          {/*          5) src/App.tsx*/}
+          {/*                       const App = () => {*/}
+          {/*                       return (*/}
+          {/*                       <div>*/}
+          {/*                       App*/}
+          {/*                       </div>*/}
+          {/*                        );*/}
+          {/*        };*/}
+          {/*          export {App};*/}
+          {/*          7) tsconfig.json-> "strictNullChecks": false*/}
+        </div>
+    );
+};
+
+export {App};
